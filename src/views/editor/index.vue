8c9d93b021ddd7ae17741a1e21831577c1082392
魏建军 <weijj@yiche.com>
2023-12-27 16:38:42 +0800

    chore: update

--- src/views/editor/index.vue
+++ src/views/editor/index.vue
@@ -30,6 +30,12 @@ import EditorContent from './components/editor-content/index.vue'
   &-container {
     position: relative;
     flex-shrink: 0;
+    background-image: radial-gradient(rgba(9, 89, 194, 0.3) 6%, transparent 0),
+      radial-gradient(#faf9f8 6%, transparent 0);
+    background-size: 10px 10px;
+    background-position:
+      0 0,
+      2px 2px;
   }
 }
 </style>
